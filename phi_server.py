6da802463aebe1b51eaff74087eb0b94e6b43127
Aditya-Sakpal <aditya.as@somaiya.edu>
2025-01-30 18:16:12 +0530

    made formatting better and added clerk middleare to phi server

--- phi_server.py
+++ phi_server.py
@@ -1,88 +1,24 @@
-import json
+import os 
 
-from phi.agent import Agent
+from dotenv import load_dotenv
 from phi.playground import Playground, serve_playground_app
 
-from utils.reserved_agents import climate_ai, green_pill_ai, owocki_ai, gitcoin_ai, reserved_agents_team
-from workflows.blog_post_generator import *
-from workflows.research_workflow import *
+from utils.get_agents_teams_workflows import load_all_agents , load_all_workflows , load_all_teams
+from middlewares.clerk_middleware import ClerkAuthMiddleware
 
-generate_blog_post = BlogPostGenerator(
-        session_id=f"generate-blog-post-on",
-        storage=SqlWorkflowStorage(
-            table_name="generate_blog_post_workflows",
-            db_file="tmp/workflows.db",
-        ),
-    )
+load_dotenv()
 
-research_workflow = ResearchWorkflow(
-    session_id="research-workflow",
-    storage=SqlWorkflowStorage(
-        table_name="research_workflows",
-        db_file="tmp/workflows.db",
-    ),
-)
-
-with open("agents.json", "r") as agent_file:
-    agents_data = json.load(agent_file)
-
-with open("teams.json", "r") as teams_file:
-    teams_data = json.load(teams_file)
-
-dynamic_agents = []
-for agent_data in agents_data:
-    dynamic_agent = Agent(
-        name=agent_data["name"],
-        agent_id=agent_data["id"],
-        user_id=agent_data["user_id"],
-        introduction=f"I am {agent_data['name']}, a {agent_data['role']}.",
-        role=agent_data["role"],
-        description=agent_data["description"],
-        instructions=agent_data["instructions"],
-        task=f"Perform the tasks outlined in the instructions for {agent_data['name']}.",
-        search_knowledge=True,
-        stream=agent_data.get("markdown", False),
-    )
-    dynamic_agents.append(dynamic_agent)
-
-dynamic_teams = []
-for team_data in teams_data:
-    team_agents = []
-    for agent_data in team_data["agents"]:
-        team_agent = Agent(
-            agent_id=agent_data["id"],
-            name=agent_data["name"],
-            introduction=f"I am {agent_data['name']}, a {agent_data['role']}.",
-            role=agent_data["role"],
-            description=agent_data["description"],
-            instructions=agent_data["instructions"],
-            task=f"Perform the tasks outlined in the instructions for {agent_data['name']}.",
-            search_knowledge=True,
-            stream=agent_data.get("markdown", False),
-        )
-        team_agents.append(team_agent)
-    
-    team_agent = Agent(
-        agent_id=team_data["id"],
-        name=team_data["name"],
-        team=team_agents,
-        instructions=team_data["instructions"],
-        role="Team",
-        description=f"The {team_data['name']} consists of multiple agents collaborating to achieve shared goals.",
-        task=f"Collaborate as a team to achieve the goals specified for {team_data['name']}.",
-        stream=team_data.get("markdown", False),
-        show_tool_calls=team_data.get("show_tool_calls", True),
-    )
-    dynamic_teams.append(team_agent)
-
-all_agents = [climate_ai, green_pill_ai, owocki_ai, gitcoin_ai, reserved_agents_team] + dynamic_agents + dynamic_teams
+all_agents = load_all_agents()
+all_teams = load_all_teams()
+all_workflows = load_all_workflows()
 
 playground_instance = Playground(
-    agents=all_agents, 
-    workflows=[generate_blog_post, research_workflow]
+    agents=all_agents+all_teams, 
+    workflows=all_workflows,
 )
 
 playground = playground_instance.get_app()
+playground.add_middleware(ClerkAuthMiddleware, api_key=os.getenv("CLERK_API_KEY"))
 
 if __name__ == "__main__":
     serve_playground_app("phi_server:playground", host="0.0.0.0", reload=True)
---
+++ utils/get_agents_teams_workflows.py
@@ -0,0 +1,109 @@
+from utils.reserved_agents import climate_ai, green_pill_ai, owocki_ai, gitcoin_ai, reserved_agents_team
+from workflows.blog_post_generator import *
+from workflows.research_workflow import *
+
+generate_blog_post = BlogPostGenerator(
+        session_id=f"generate-blog-post-on",
+        storage=SqlWorkflowStorage(
+            table_name="generate_blog_post_workflows",
+            db_file="tmp/workflows.db",
+        ),
+    )
+
+research_workflow = ResearchWorkflow(
+    session_id="research-workflow",
+    storage=SqlWorkflowStorage(
+        table_name="research_workflows",
+        db_file="tmp/workflows.db",
+    ),
+)
+
+def load_all_workflows():
+    """
+    Load all workflows
+    
+    Returns:
+        list: List of workflows
+    """
+    return [generate_blog_post, research_workflow]
+
+def load_all_agents():
+    """
+    Load all agents 
+    
+    Returns:
+        list: List of agents
+    """
+    with open("agents.json", "r") as agent_file:
+        agents_data = json.load(agent_file)
+
+    dynamic_agents = []
+    for agent_data in agents_data:
+        dynamic_agent = Agent(
+            name=agent_data["name"],
+            agent_id=agent_data["id"],
+            user_id=agent_data["user_id"],
+            introduction=f"I am {agent_data['name']}, a {agent_data['role']}.",
+            role=agent_data["role"],
+            description=agent_data["description"],
+            instructions=agent_data["instructions"],
+            task=f"Perform the tasks outlined in the instructions for {agent_data['name']}.",
+            search_knowledge=True,
+            stream=agent_data.get("markdown", False),
+        )
+        dynamic_agents.append(dynamic_agent)
+        
+    all_agents = [
+        climate_ai,
+        green_pill_ai,
+        owocki_ai,
+        gitcoin_ai,
+    ] + dynamic_agents
+    
+    return all_agents
+
+def load_all_teams():
+    """
+    Load all teams
+    
+    Returns:
+        list: List of teams
+    """
+    with open("teams.json", "r") as teams_file:
+        teams_data = json.load(teams_file)
+
+    dynamic_teams = []
+    for team_data in teams_data:
+        team_agents = []
+        for agent_data in team_data["agents"]:
+            team_agent = Agent(
+                agent_id=agent_data["id"],
+                name=agent_data["name"],
+                introduction=f"I am {agent_data['name']}, a {agent_data['role']}.",
+                role=agent_data["role"],
+                description=agent_data["description"],
+                instructions=agent_data["instructions"],
+                task=f"Perform the tasks outlined in the instructions for {agent_data['name']}.",
+                search_knowledge=True,
+                stream=agent_data.get("markdown", False),
+            )
+            team_agents.append(team_agent)
+    
+        team_agent = Agent(
+            agent_id=team_data["id"],
+            name=team_data["name"],
+            team=team_agents,
+            instructions=team_data["instructions"],
+            role="Team",
+            description=f"The {team_data['name']} consists of multiple agents collaborating to achieve shared goals.",
+            task=f"Collaborate as a team to achieve the goals specified for {team_data['name']}.",
+            stream=team_data.get("markdown", False),
+            show_tool_calls=team_data.get("show_tool_calls", True),
+        )
+        dynamic_teams.append(team_agent)
+        
+    all_teams = [
+        reserved_agents_team,
+    ] + dynamic_teams
+    
+    return all_teams
