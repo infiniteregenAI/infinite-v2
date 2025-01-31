2dd2c7282bafdf8e0bbcae59b4c21ac16f497edc
Aditya-Sakpal <aditya.as@somaiya.edu>
2025-01-31 17:58:18 +0530

    added cors middleware

--- phi_server.py
+++ phi_server.py
@@ -2,10 +2,12 @@ import os
 
 from dotenv import load_dotenv
 from phi.playground import Playground, serve_playground_app
+from fastapi.middleware.cors import CORSMiddleware
 
 from utils.get_agents_teams_workflows import load_all_agents , load_all_workflows , load_all_teams
 from middlewares.clerk_middleware import ClerkAuthMiddleware
 from schemas.database import init_db
+from utils.constants import ALLOWED_ORIGINS
 
 load_dotenv()
 init_db()
@@ -21,6 +23,14 @@ playground_instance = Playground(
 
 playground = playground_instance.get_app()
 playground.add_middleware(ClerkAuthMiddleware, api_key=os.getenv("CLERK_SECRET_KEY"))
+playground.add_middleware(
+    CORSMiddleware,
+    allow_origins=ALLOWED_ORIGINS, 
+    allow_credentials=True,  
+    allow_methods=["*"],  
+    allow_headers=["*"], 
+)
+
 
 if __name__ == "__main__":
     serve_playground_app("phi_server:playground", host="0.0.0.0", reload=True)
