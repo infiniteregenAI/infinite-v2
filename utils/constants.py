a194f20a51bb1edee2ce445cc3495f6dc14fdf72
dharamveergit <dharamveersingh.em@gmail.com>
2025-02-01 00:18:44 +0530

    Add Vercel deployment URL to allowed CORS origins

--- utils/constants.py
+++ utils/constants.py
@@ -1,6 +1,7 @@
 AVAILABLE_TOOLS = ["HackerNews", "DuckDuckGo", "Newspaper4k"]
 CLERK_ALLOWED_PARTIES = [""]
 ALLOWED_ORIGINS = [
+    "https://infiniteregen-dash.vercel.app",
     "http://localhost:3000",    # Next.js development
     "http://127.0.0.1:3000",    # Alternative Next.js URL
     "http://localhost:7777",    # Phi server
